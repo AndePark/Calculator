54c799b594a015d1e7ba6962e98f9542a1a8be86
Andrew Park <andrewpark2396@gmail.com>
2023-01-04 22:52:04 -0800

    Adding functionality to buttons

--- javascript.js
+++ javascript.js
@@ -1,19 +1,120 @@
-const one = document.querySelector('#one'); 
-const two = document.querySelector('#two');
 
-const equals = document.querySelector('#equals');
-const addition = document.querySelector('#add');
+// variable number is an array of all numbered buttons 
+// const number = Array.from(document.querySelectorAll('div.numbers > input'));
+// // below will display array of button.num but NOT the individual values 
+// console.log(number);
 
-let number = {
-  one: 1,
-  two: 2,
-}
+let x;
+// let y=3;
+
+
+
+// nodelist of all numbered buttons 
+const buttons = Array.from(document.querySelectorAll('div.numbers > input'));
+
+let b = [];
+
+buttons.forEach((button) => {
+  b.push(button.defaultValue);
+});
+
+console.log(b);
+
+
+
+
+
+// console.log(buttons);
+
+
+// we add an eventlistener for each of the buttons 
+// buttons.forEach((button) => {
+//   button.addEventListener('click', function(e) {
+//     // x = e.target.value;
+//   });
+// });
+
+
+// function values(v) {
+//   x = buttons[v];
+// }
+
+
+// function values(e) {
+//   x = e;
+// }
+
+console.log(x);
+
+
+// buttons.forEach((button) => {
+//   button.addEventListener('click', function(e) {
+//     y = e.target.value;
+//   });
+//   });
+
+//   let y;
+
+  // function values(e) {
+  //   if (x == null) {
+  //     x = 
+  //   }
+  // }
+
+
+
+// console.log(x);
+// console.log(y);
+
+
+
+// the following prints out the value of the button pressed 
+// btn.addEventListener('click', function(e) {
+//   console.log(e.target.value);
+// });
+
+
+// const two = document.querySelector('#two');
+
+// console.log(number);
+
+// let x = Number;
+
+// function values() {
+//   alert(number);
+//   }
+
+// console.log(one);
+
+// const log = document.getElementById('answer');
+
+
+
+
+// const equals = document.querySelector('#equals');
+// const addition = document.querySelector('#add');
+
+// const textBox = alert();
+
+// textBox.addEventListener('')
+
+// if i use an array for all operators 
+// const ope = document.querySelectorAll('#op');
+// console.log(ope);
+
+
+// convert the nodelist to array 
+
+// let number = {
+//   one: 1,
+//   two: 2,
+// }
 
 let operator = {
   addition: add
 }
 
-equals.onclick = () => alert(operate(operator.addition, number.one, number.two));
+// equals.onclick = () => alert(operate(operator.addition, number.one, number.two));
 
 
 
